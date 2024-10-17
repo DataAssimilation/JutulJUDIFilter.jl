6b2c012ba95f8282732c49e0c3bdbccbac0b7da2
Grant Bruer <grant.git.email@gmail.com>
2024-10-17 18:41:16 -0400

    Missing file

---
+++ src/installs.jl
@@ -0,0 +1,16 @@
+
+using Pkg: Pkg
+_dependencies = Dict{Symbol,Any}()
+_dependencies[:ConfigurationsJutulDarcy] =
+    () -> Pkg.add(; url="https://github.com/tmp398243/tmp395832")
+_dependencies[:ConfigurationsJUDI] =
+    () -> Pkg.add(; url="https://github.com/tmp398243/tmp3117499")
+_dependencies[:Ensembles] =
+    () -> Pkg.add(; url="https://github.com/tmp398243/tmp32487543")
+
+function install(pkg::Symbol)
+    if !(pkg in keys(_dependencies))
+        error("Unknown package: $pkg")
+    end
+    return _dependencies[pkg]()
+end
