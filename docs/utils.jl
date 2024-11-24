84d9007001c8226e41bb2243afff4694290fa290
Grant Bruer <grant.git.email@gmail.com>
2024-11-24 16:55:52 -0500

    Fixed examples coverage (except for main.jl)

--- docs/utils.jl
+++ docs/utils.jl
@@ -17,3 +17,33 @@ function add_extra_info(content, pth; build_notebooks, build_scripts)
         # Notebook viewer may work [here](https://nbviewer.org/urls/dataassimilation.github.io/JutulJUDIFilter.jl/$(joinpath("examples", pth, "main.ipynb")))
     """
 end
+
+using Base.Filesystem: checkfor_mv_cp_cptree, mkdir, readdir, islink, symlink, readlink, sendfile
+function cptree_regex(src::String, dst::String;
+                                          follow_symlinks::Bool=false,
+                                          ignore_hidden::Bool=false,
+                                          match::Union{Nothing, Regex}=nothing)
+    isdir(src) || throw(ArgumentError("'$src' is not a directory. Use `cp(src, dst)`"))
+    for name in readdir(src)
+        if ignore_hidden && name[1] == '.'
+            continue
+        end
+        srcname = joinpath(src, name)
+        if !follow_symlinks && islink(srcname)
+            if isnothing(match) || occursin(match, srcname)
+                mkpath(dst)
+                symlink(readlink(srcname), joinpath(dst, name))
+            end
+        elseif isdir(srcname)
+            cptree_regex(srcname, joinpath(dst, name); follow_symlinks, ignore_hidden, match)
+        else
+            check = isnothing(match) || occursin(match, srcname)
+            if check
+                mkpath(dst)
+                sendfile(srcname, joinpath(dst, name))
+            end
+        end
+    end
+end
+cptree_regex(src::AbstractString, dst::AbstractString; kwargs...) =
+    cptree_regex(String(src)::String, String(dst)::String; kwargs...)
